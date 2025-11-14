058b3b78ea637c6e5afb9d0685730981e2dde7b6
angelofarias <angelo.evang@gmail.com>
2025-11-13 23:45:35 -0300

    diretorios

--- Ada/TodoApp/Program.cs
+++ Ada/TodoApp/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace TodoApp
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TodoApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Criar um Builder de uma Web Application (Web Application é complexa -> Então usados o padrão
+            //  Builder)
+            var builder = WebApplication.CreateBuilder();
+
+            // Configurar serviços (Injeção de Dependência)
+            builder.Services.AddControllers();
+
+            // Constroi a aplicação (builder.Build())
+            var app = builder.Build();
+
+            app.MapControllers();
+
+            // Roda aplicação
+            app.Run();
         }
     }
 }
